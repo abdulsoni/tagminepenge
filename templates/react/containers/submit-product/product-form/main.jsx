efd433e883470f1082f4c588e1115d4823a75abe
Jasjot Singh <jasjot.singh@laxaar.com>
2017-11-14 13:18:02 +0530

    fixes

--- templates/react/containers/submit-product/product-form/main.jsx
+++ templates/react/containers/submit-product/product-form/main.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import ComponentView from './view';
 import { connect } from 'react-redux';
 import { sampleAction } from '../../../redux/actions/index';
+import {handleChange} from '../../../utils/common';
+import {blobToBlobURL} from '../../../utils/file';
 /**
  * @name Sample Container
  * @type Container
@@ -14,8 +16,70 @@ class Main extends Component {
 	 */
 	constructor(props){
 		super(props);
+		console.log(props)
+		this.initialState = {
+			"productTitle" : "",
+			"productDescription" : "",
+			"productMessage" : "",
+			"link" : "",
+			"price" : "",
+			"loading" : false,
+			"message" : null,
+			fileUrl : null,
+			file : null
+		};
+		this.state = this.initialState;
+		this.handleChange = handleChange.bind(this)
+	}
+	submit(event){
+		event.preventDefault();
+		const {productForm} = this.props;
+		const {productTitle,productDescription,link,price} = this.state;
+		this.setState({
+			loading : true
+		});
+		productForm({
+			productTitle,productDescription,link,price,message : productMessage
+		}).then(action=>{
+			console.log(action);
+			if(action.error){
+				this.setState({
+					loading : false,
+					message : {
+						type : "danger",
+						text : "Error while sending your message"
+					}
+				});
+			} else {
+				this.reset();
+				this.setState({
+					loading : false,
+					message : {
+						type : "success",
+						text : "We have received your message. We will get back to you shortly"
+					}
+				});
+			}
+		})
+
+	}
+	reset(){
+		this.setState(this.initialState)
 	}
 
+	/**
+	 * on File Change
+	 */
+	onFileChange(file,duration){
+		//console.log(duration)
+		this.setState({
+			file,
+			fileUrl : blobToBlobURL(file),
+			isVideo : this.isVideo(file),
+			duration : duration || [0,0]
+		})
+	}
+	
 	/**
 	 * Render the view
 	 */
--- templates/react/containers/submit-product/product-form/view.jsx
+++ templates/react/containers/submit-product/product-form/view.jsx
@@ -1,14 +1,101 @@
 import React from 'react';
 import styles from './style.scss';
-
+import ImagePicker from '../../../components/image-picker';
 var view = function () {
+	const {handleChange, submit, state} = this;
+	const {config} = this.props;
+	const {loading, message, fileUrl,file} = this.state;
 	return (
 		<div className="step step-two">
 			<div className="container">
 				<img src="/images/step2.png" alt="step"/>
-				<form>
-					
-				</form>
+				<div className="row form">
+					<div className="col-md-6">
+						<form onSubmit={submit.bind(this)}>
+							{
+								message ? (
+									<div className={"alert alert-" + message.type}>
+										{message.text}
+									</div>
+
+								) : null
+							}
+							<div className="form-group">
+								<label>Product Title <span className="length">(max 40 characters)</span></label>
+								<input readOnly={loading}
+									   type="text"
+									   onChange={handleChange.bind(this)}
+									   value={this.state.productTitle}
+									   name="productTitle"
+									   className="form-control"
+									   required
+								/>
+							</div>
+							<div className="form-group">
+								<label>Product Description <span className="length">(max 250 characters)</span></label>
+								<textarea readOnly={loading}
+										  className="form-control"
+										  onChange={handleChange.bind(this)}
+										  value={this.state.productDescription}
+										  name="productDescription"
+										  required
+								/>
+							</div>
+							<div className="form-group">
+								<label>Link</label>
+								<input readOnly={loading}
+									   type="text"
+									   onChange={handleChange.bind(this)}
+									   value={this.state.link}
+									   name="link"
+									   className="form-control"
+									   required
+								/>
+							</div>
+							<div className="form-group">
+								<label>Price</label>
+								<input readOnly={loading}
+									   type="number"
+									   onChange={handleChange.bind(this)}
+									   value={this.state.price}
+									   name="price"
+									   className="form-control"
+									   required
+								/>
+							</div>
+							<div className="form-group">
+								<label>Image Upload</label>
+								<ImagePicker
+									file={file}
+									fileUrl={fileUrl}
+									accept="video/mp4,video/x-m4v,image/*,video/*"
+									crop={true}
+									cropWidth={'100%'}
+									cropHeight={332}
+									onFileChange={this.onFileChange.bind(this)}
+									onSave={this.onFileChange.bind(this)}>
+									{
+										fileUrl ? (
+											<div className="cropped-img">
+												<img src={fileUrl}/>
+											</div>
+										) : (
+											<div className="upload-btn">
+												<input type="file" name="file" />
+											</div>
+										)
+									}
+								</ImagePicker>
+							</div>
+							<button disabled={loading} type="submit" className="btn btn-yellow pull-right">Save and
+								Continue
+							</button>
+						</form>
+					</div>
+					<div className="col-md-6">
+						
+					</div>
+				</div>
 			</div>
 		</div>
 	);
