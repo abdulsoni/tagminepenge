91adaaee2c8227f67da076d02151ee550a99543a
Inderdeep Singh <inderdeep.singh@laxaar.com>
2017-11-16 14:21:10 +0530

    DB Restore fix

--- db-restore.sh
+++ db-restore.sh
@@ -1,4 +1,6 @@
 mongodump --db tagminepenge-cms
 cd dump/
 mv tagminepenge-cms/ dump
+mongo ds243325.mlab.com:43325/heroku_4p6q9gwj -u heroku_4p6q9gwj -p 3vkbmr6vk2k8vn21s4keeja02i --eval  "db.getCollectionNames().forEach(function(n){db[n].remove({})});"
 mongorestore -h ds243325.mlab.com:43325 -d heroku_4p6q9gwj -u heroku_4p6q9gwj -p 3vkbmr6vk2k8vn21s4keeja02i
+rm -r -f dump
