2c5e0c6015b532e80714b33f5f442727880f88f9
Inderdeep Singh <inderdeep.singh@laxaar.com>
2017-11-21 18:30:44 +0530

    Fixes

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#patrick_knudsen78@hotmail.com/jamdido34zamz!
+#patrick_knudsen78@hotmail.com/jamdido34zam!
 heroku config:set NPM_CONFIG_PRODUCTION=false --app tagminepenge
 git remote remove heroku-prod
 git remote add heroku-prod https://git.heroku.com/tagminepenge.git
