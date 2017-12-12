b23cc06deae15b397ce9bef1500ad54cadd29506
Protolabz e Service <Patrick_knudsen78@hotmail.com>
2017-12-12 14:18:39 +0530

    make it better

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -48208,6 +48208,7 @@
 				_react2.default.createElement('link', { rel: 'stylesheet', href: 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' }),
 				_react2.default.createElement('script', { src: 'https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js' }),
 				_react2.default.createElement('script', { src: 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js' }),
+				_react2.default.createElement('style', { dangerouslySetInnerHTML: { __html: props.children } }),
 				props.user && props.user.canAccessKeystone && _react2.default.createElement('link', { href: '/keystone/styles/content/editor.min.css', rel: 'stylesheet' }),
 				props.css,
 				props.head
--- templates/layouts/default.jsx
+++ templates/layouts/default.jsx
@@ -22,7 +22,7 @@ const Default = props => {
 			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"/>
 				<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 				<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
+			<style dangerouslySetInnerHTML={{__html: props.children}} />
 			{props.user && props.user.canAccessKeystone
 			&& <link href="/keystone/styles/content/editor.min.css" rel="stylesheet"/>
 			}
